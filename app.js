573a6592fc46c28b29da69f0778d33e0381391a7
ABDULLAH GHANI <abdullah.ghani3@gmail.com>
2016-08-08 10:47:02 +0530

    changes

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var data = [
   },
   {
     name:'MOVIE SEARCH',
-    url:'http://ec2-52-39-216-105.us-west-2.compute.amazonaws.com:3000/',
+    url:'http://ec2-52-24-14-35.us-west-2.compute.amazonaws.com:3000/',
     github:'https://github.com/abdghani/angular2_imdb_app.git',
     description:'An angular 2 app . Get details of any MOVIE . Made using Omdb API',
     image:'imdb.png'
