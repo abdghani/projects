07a7db7b4f2cc7b6ead69aad2710d14f54208d80
Abdullah Ghani <abdullahghani@Abdullahs-MacBook-Pro.local>
2017-03-08 11:07:44 +0530

    updates

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var app = angular.module('projects',[]);
 
 var data = [
   {
-    name:'C-IDE',
-    url:'https://c-ide.herokuapp.com/',
-    github:'https://github.com/abdghani/ONLINE-C-IDE.git',
-    description:'A simple And easy to use c IDE made using node and angular js. Begin coding in c with just your browser',
-    image:'cide.png'
+    name:'P-IDE',
+    url:'http://www.thegreyphase.info/',
+    github:'https://github.com/abdghani/p-ide.git',
+    description:'An online IDE for python where one can run machine learning codes using pandas,numpy,scipy,etc Backend hosted at amazon ec2',
+    image:'makepost.png'
   },
   {
     name:'SMSANY',
@@ -16,12 +16,18 @@ var data = [
     image:'cides.png'
   },
   {
-    name:'P-IDE',
-    url:'http://www.thegreyphase.info/',
-    github:'https://github.com/abdghani/p-ide.git',
-    description:'An online IDE for python where one can run machine learning codes using pandas,numpy,scipy,etc Backend hosted at amazon ec2',
-    image:'makepost.png'
+    name:'C-IDE',
+    url:'https://c-ide.herokuapp.com/',
+    github:'https://github.com/abdghani/ONLINE-C-IDE.git',
+    description:'A simple And easy to use c IDE made using node and angular js. Begin coding in c with just your browser',
+    image:'cide.png'
   },
+  {
+    name:'ANGULAR2GITHUB',
+    url:'http://104.236.87.227:3000/'
+    github:'https://github.com/abdghani/angular2imdb',
+    description:'Search for any movie on imdb. Made completely using angular 2'
+  }
   {
     name:'GITHUB INFO',
     url:'https://githubinfo.herokuapp.com/',
@@ -43,13 +49,12 @@ var data = [
     description:'A Standand REST api based mean stack website. Login using passport js in facebook . Store bookmark from any device and open it from anywhere . Maintain all your todos and never be late for anything . Easy to use interface with realtime bookmark share',
     image:'bookmark.png'
   },
-//   {
-//     name:'MOVIE SEARCH',
-//     url:'http://ec2-52-66-22-195.ap-south-1.compute.amazonaws.com:3000/',
-//     github:'https://github.com/abdghani/angular2_imdb_app.git',
-//     description:'An angular 2 app . Get details of any MOVIE . Made using Omdb API',
-//     image:'imdb.png'
-//   }
+  {
+    name:'Tweetify',
+    url:'http://tweetsearch.bitballoon.com/',
+    github:'https://github.com/abdghani/tweety',
+    description:'Sentiment analysis on twitter feed.'
+  }
 ];
 
 var skills = [
