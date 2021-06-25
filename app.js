085c1d68ba744b4861f24852cc9eada4cda5cb04
abdullah ghani <abdullah.ghani3@gmail.com>
2021-06-25 08:41:29 +0530

    changes

--- app.js
+++ app.js
@@ -1,86 +1,31 @@
-var app = angular.module('projects',[]);
+var app = angular.module('projects', []);
 
-var data = [
-  {
-    name:'NEWZY',
-    url:'http://newzy.xyz',
-    github:'https://github.com/abdghani/newzy/tree/master/front',
-    description:'A live news app for fetching news from various online resources and user-user based colaborative filtering',
-    image:'newzy.png'
-  },
-  {
-    name:'P-IDE',
-    url:'http://pide.greyphase.xyz',
-    github:'https://github.com/abdghani/pIDE',
-    description:'An online IDE for python where one can run machine learning codes using tensorflow,pandas,numpy,scipy,etc . Build fully on MEAN stack with socket implemetation . Supports Online Code Sharing with live coding Editing with url sharing. ',
-    image:'pide.png'
-  },
-  {
-    name:'POSify',
-    url:'http://posify.greyphase.xyz/',
-    github:'https://github.com/abdghani/posify',
-    description:'A parts of speech tagging application with language transaltion Using NLTK and TextBlob',
-    image:'posify.png'
-  },
-  {
-    name:'FREELANCE',
-    url:'http://freelance.greyphase.xyz',
-    github:'https://github.com/IamAKX/Freelance',
-    description:'An android app for freelance user with native implemetation and nodejs as a backend language . Redis for session management and mongo as a data storage. Download the app at http://freelance.greyphase.xyz/#!/app',
-    image:'freelance.png'
-  },
-  {
-    name:'SMSANY',
-    url:'http://smsany.greyphase.xyz/',
-    github:'https://github.com/abdghani/smsany',
-    description:'A simple mobile push message service using Amazon sns . Can be included as a service with any platfor by user authentication  ',
-    image:'smsany.png'
-  },
-  {
-    name:'EXPRESS TODO',
-    url:'http://todos.greyphase.xyz/',
-    github:'https://github.com/abdghani/expressTodo.git',
-    description:'A Standand REST api based mean stack website. Login using passport js in facebook . Store bookmark from any device and open it from anywhere . Maintain all your todos and never be late for anything . Easy to use interface with realtime bookmark share',
-    image:'bookmark.png'
-  },
-  {
-    name:'MOVIEREC',
-    github:'https://github.com/abdghani/recomender',
-    description:'A movie recommender system based on K means clustering',
-    // image:'bookmark.png'
-  },
-  {
-    name:'C-IDE',
-    url:'http://cide.greyphase.xyz/',
-    github:'https://github.com/abdghani/c-ide',
-    description:'A simple And easy to use c IDE made using node and angular js. Begin coding in c with just your browser',
-    image:'cide.png'
-  },
-  {
-    name:'GITHUB INFO',
-    url:'http://gitinfo.greyphase.xyz/',
-    github:'https://github.com/abdghani/gitinfo.git',
-    description:'Search for anybody with there username in github',
-    image:'githubinfo.png'
-  }
-];
+var data = [{
+  name: 'SURVAIDER',
+  url: 'http://survaider.com',
+  description: 'Core memeber in development of the platform',
+}, {
+  name: 'ZAREAM',
+  url: 'https://zaream.greyphase.xyz/',
+  description: 'A platform to cluster similar news together using web scrapping and ML. ',
+}, {
+  name: 'VANTAGE NEWS',
+  url: 'https://drive.google.com/file/d/1kIjwYbFhYO2vYokmW19fV18VtPuKEWyO/view?usp=sharing',
+  description: 'A mobile application of news made using Flutter.',
+}, ];
 
-var skills = [
-  {
-    name:'JAVASCRIPT (MEAN STACK)',
-    level:85
-  },
-  {
-    name:'PHP (LAMP STACK)',
-    level:80
-  },
-  {
-    name:'DJANGO',
-    level:50
-  },
-  {
-    name:'c++, java, python',
-    level:70
+var skills = [{
+    name: 'JAVASCRIPT (MEAN STACK)',
+    level: 85
+  }, {
+    name: 'PHP (LAMP STACK)',
+    level: 80
+  }, {
+    name: 'DJANGO',
+    level: 50
+  }, {
+    name: 'c++, java, python',
+    level: 70
   },
 
 
@@ -88,16 +33,13 @@ var skills = [
 
 var funs = [
   'Nodejs',
-  'AngularJs',
+  'Angular',
   'MongoDb',
-  'Loopback',
   'Python',
-  'Flask',
-  'Javascript',
-  'AWS','Digital Ocean',
 ]
-app.controller("project_controller",function($scope){
+
+app.controller("project_controller", function($scope) {
   $scope.projs = data;
   $scope.skill = skills;
-  $scope.funs= funs;
+  $scope.funs = funs;
 });
